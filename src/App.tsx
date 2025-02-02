03f60168e4a88ef485a02920f892372488519cca
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-02 11:09:09 +0100

    36

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import CourseGoal from "./components/CourseGoal"
 import Header from "./components/Header"
 import goalsImg from './assets/goals.jpg'
+import { useState } from "react"
+
+
+type CourseGoal = {
+  title: string;
+  description:string;
+  id:number;
+}
 
 function App() {
+
+const [goals, setGoals]=  useState<CourseGoal[]>([]);
+
+  function handleAddGola(){
+
+  }
 
   return (
     <>
@@ -11,6 +25,7 @@ function App() {
           <Header src={goalsImg} alt="A list of goals">
              <h1>Your Course Goals</h1>
           </Header>
+          <button onClick={handleAddGola}>Add Goal</button>
           <CourseGoal
         title="Esto es el titulo"
         >
