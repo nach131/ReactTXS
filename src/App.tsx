abef2a2ce69b50c31e54987367195bf2b69e4611
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-02 11:22:16 +0100

    37

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,46 @@
-import CourseGoal from "./components/CourseGoal"
-import Header from "./components/Header"
-import goalsImg from './assets/goals.jpg'
-import { useState } from "react"
-
+import CourseGoal from './components/CourseGoal';
+import Header from './components/Header';
+import goalsImg from './assets/goals.jpg';
+import { useState } from 'react';
 
 type CourseGoal = {
   title: string;
-  description:string;
-  id:number;
-}
+  description: string;
+  id: number;
+};
 
 function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
 
-const [goals, setGoals]=  useState<CourseGoal[]>([]);
-
-  function handleAddGola(){
-
+  function handleAddGola() {
+    setGoals((prev) => {
+      const newGols: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn react',
+        description: ' descripcion de learn',
+      };
+      return [...prev, newGols];
+    });
   }
 
   return (
     <>
       <main>
-          {/* <Header image={{src: goalsImg, alt: "A list of goals"}}> */}
-          <Header src={goalsImg} alt="A list of goals">
-             <h1>Your Course Goals</h1>
-          </Header>
-          <button onClick={handleAddGola}>Add Goal</button>
-          <CourseGoal
-        title="Esto es el titulo"
-        >
-          Descripcion desde children
-        </CourseGoal>
+        {/* <Header image={{src: goalsImg, alt: "A list of goals"}}> */}
+        <Header src={goalsImg} alt="A list of goals">
+          <h1>Your Course Goals</h1>
+        </Header>
+        <button onClick={handleAddGola}>Add Goal</button>
+        <ul>
+          {goals.map((goal) => (
+            <li key={goal.id}>
+              <CourseGoal title={goal.title}>{goal.description}</CourseGoal>
+            </li>
+          ))}
+        </ul>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
