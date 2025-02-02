db8948d4489c4c2c6e8428b37c768f01bd0cd9bf
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-02 19:06:30 +0100

    42

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,19 +1,25 @@
-import { FormEvent } from 'react';
+import { useRef, type FormEvent } from 'react';
 
 export default function NewGoal() {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
   function handleSumbit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
   }
 
   return (
     <form onSubmit={handleSumbit}>
       <p>
         <label htmlFor="goal">You Goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" ref={goal} />
       </p>
       <p>
         <label htmlFor="sumary">Short summary</label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" ref={summary} />
       </p>
       <p>
         <button>Add Goal</button>
