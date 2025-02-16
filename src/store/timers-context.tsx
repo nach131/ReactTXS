44d35405b9c94e1ea6ce6da0c4e04ec75f533b52
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-16 19:19:41 +0100

    66

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, type ReactNode } from 'react';
 
 type Timer = {
   name: string;
@@ -12,8 +12,33 @@ type TimersState = {
 
 type TimersContexValue = TimersState & {
   addTimer: (timerData: Timer) => void;
-  startTimer: () => void;
+  startTimers: () => void;
   stopTimer: () => void;
 };
 
 const TimersContext = createContext<TimersContexValue | null>(null);
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContexValue = {
+    timers: [],
+    isRunning: false,
+    addTimer(timerData) {
+      // ...
+    },
+    startTimers() {
+      // ..
+    },
+    stopTimer() {
+      // ..
+    },
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
