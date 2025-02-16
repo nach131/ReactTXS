2534abe7503cea5dd4074012406b11b969b29739
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-16 12:17:40 +0100

    58

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import Input from './components/Input';
 import { useRef } from 'react';
+import Form from './components/Form';
+import Button from './components/Button';
 
 function App() {
   const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input id="Test" label="Test" type="text" ref={input} />
+      <Form>
+        <Input id="text" label="Name" type="text" ref={input} />
+        <Input id="number" label="Age" type="number" ref={input} />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
