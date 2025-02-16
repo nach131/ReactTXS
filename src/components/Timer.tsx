bd14be11e713619a71c6d540cea5b2ff108b0b21
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-16 23:27:01 +0100

    72

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,9 +1,12 @@
 import Container from './UI/Container.tsx';
 
-export default function Timer() {
+import { type Timer as TimerProps } from '../store/timers-context.tsx';
+
+export default function Timer({ name, duration }: TimerProps) {
   return (
     <Container as="article">
-      <h2>TODO: TIMER NAME</h2>
+      <h2>{name}</h2>
+      <p>{duration}</p>
     </Container>
   );
 }
