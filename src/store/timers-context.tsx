4bb668de5e2751c7c703b796c2119cc6d2e4153a
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-16 18:40:23 +0100

    65

---
+++ src/store/timers-context.tsx
@@ -0,0 +1,19 @@
+import { createContext } from 'react';
+
+type Timer = {
+  name: string;
+  duration: number;
+};
+
+type TimersState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+type TimersContexValue = TimersState & {
+  addTimer: (timerData: Timer) => void;
+  startTimer: () => void;
+  stopTimer: () => void;
+};
+
+const TimersContext = createContext<TimersContexValue | null>(null);
