d3f3c318b5fb397f03e1e6312237e5af74385128
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-17 19:35:21 +0100

    74

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,21 @@
 import Container from './UI/Container.tsx';
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useState } from 'react';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingState, setRemainingTime] = useState(duration * 1000);
+
+  setInterval(function () {
+    setRemainingTime((prevTime) => prevTime - 50);
+  }, 50);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={reainingState} />
+      </p>
+      <p>{}</p>
     </Container>
   );
 }
