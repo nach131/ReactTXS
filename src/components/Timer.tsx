8db18319ee751a92cee6b18df328addd11e9b22d
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2025-02-23 18:37:09 +0100

    77

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,24 +1,31 @@
 import { useState, useRef, useEffect } from 'react'; // Añadido useEffect
 import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from '../store/timers-context.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
   const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000); // Corregido el nombre de la variable
+  const { isRunning } = useTimersContext();
 
   if (remainingTime <= 0 && interval.current) {
     clearInterval(interval.current);
   }
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setRemainingTime((prevTime) => Math.max(prevTime - 50, 0)); // Añadida prevención para valores negativos
-    }, 50);
-
-    interval.current = timer;
-
-    return () => clearInterval(timer); // Limpieza del intervalo
-  }, []);
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => Math.max(prevTime - 50, 0)); // Añadida prevención para valores negativos
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current); // Limpieza del intervalo
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
